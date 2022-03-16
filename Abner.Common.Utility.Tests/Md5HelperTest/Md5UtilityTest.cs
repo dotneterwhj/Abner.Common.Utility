5cb57a8009f422d6b2c910dea8e46598dc16e61f
wuhuajie <17681550310@163.com>
2022-03-16 16:02:17 +0800

    Md5Helper

---
+++ Abner.Common.Utility.Tests/Md5HelperTest/Md5UtilityTest.cs
@@ -0,0 +1,26 @@
+using Xunit;
+using Abner.Common.Utility;
+using System.IO;
+
+namespace Abner.Common.Utility.Tests;
+
+public class Md5UtilityTest
+{
+    [Fact]
+    public void ComputeMd5()
+    {
+        Assert.Equal("34b577be20fbc15477aadb9a08101ff9", Md5Utility.ComputeMd5("string1"));
+        Assert.Equal("20fbc15477aadb9a", Md5Utility.ComputeMd5("string1", Bits.Sixteen));
+        Assert.Equal("77aadb9a", Md5Utility.ComputeMd5("string1", Bits.Eight));
+
+        Assert.Throws<FileNotFoundException>(() =>
+        {
+            Md5Utility.ComputeMd5(new FileInfo(@"file not exist"));
+        });
+
+        Assert.NotNull(Md5Utility.ComputeMd5(new FileInfo(@"C:\snmp.etl")));
+        Assert.NotNull(Md5Utility.ComputeMd5(new FileInfo(@"C:\snmp.etl"), Bits.Sixteen));
+        Assert.NotNull(Md5Utility.ComputeMd5(new FileInfo(@"C:\snmp.etl"), Bits.Eight));
+
+    }
+}
---
+++ Abner.Common.Utility/Md5Helper/Md5Utility.cs
@@ -0,0 +1,84 @@
+using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Abner.Common.Utility
+{
+
+    public enum Bits
+    {
+        Eight,
+        Sixteen,
+        ThirtyTwo
+    }
+
+    public class Md5Utility
+    {
+
+        private static string GetString(byte[] md5Bytes, Bits bits = Bits.ThirtyTwo)
+        {
+            string md5string = string.Empty;
+
+            int start = 0, end = 16;
+            switch (bits)
+            {
+                case Bits.Eight:
+                    start = 8;
+                    end = 12;
+                    break;
+                case Bits.Sixteen:
+                    start = 4;
+                    end = 12;
+                    break;
+                case Bits.ThirtyTwo:
+                    start = 0;
+                    end = 16;
+                    break;
+                default:
+                    throw new ArgumentException($"不存在的枚举类型{bits}");
+            }
+            for (int i = start; i < end; i++)
+            {
+                md5string += md5Bytes[i].ToString("x2");
+            }
+
+            return md5string;
+        }
+
+        /// <summary>
+        /// 获取MD5字符串
+        /// </summary>
+        /// <param name="str">需要转换的字符串</param>
+        /// <param name="bits">转换的位数</param>
+        /// <returns>MD5字符串</returns>
+        public static string ComputeMd5(string str, Bits bits = Bits.ThirtyTwo)
+        {
+            MD5 md5 = MD5.Create();
+            byte[] buffer = Encoding.UTF8.GetBytes(str);
+            byte[] md5Bytes = md5.ComputeHash(buffer);
+            return GetString(md5Bytes, bits);
+        }
+
+        /// <summary>
+        /// 获取文件的MD5值
+        /// </summary>
+        /// <param name="file">指定的文件</param>
+        /// <returns>MD5字符串</returns>
+        public static string ComputeMd5(FileInfo file, Bits bits = Bits.ThirtyTwo)
+        {
+            if (!file.Exists)
+            {
+                throw new FileNotFoundException();
+            }
+            MD5 md5 = MD5.Create();
+            byte[] md5Bytes = null;
+            using (FileStream fsRead = new FileStream(file.FullName, FileMode.Open, FileAccess.Read))
+            {
+                md5Bytes = md5.ComputeHash(fsRead);
+            }
+
+            return GetString(md5Bytes, bits);
+        }
+    }
+}
