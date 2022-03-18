8ef44aa242def25582c99c8a8c806c39fc2baede
wuhuajie <17681550310@163.com>
2022-03-18 21:04:35 +0800

    EncryptHelper

---
+++ Abner.Common.Utility.Tests/EncyptHelperTest/EncyptUtilityTest.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Abner.Common.Utility.Tests.EncyptHelperTest
+{
+    public class EncyptUtilityTest
+    {
+        [Fact]
+        public void AESEncypt()
+        {
+            var key1 = Random.Shared.Next(343465767);
+            var key2 = Random.Shared.Next(334098343);
+
+            Assert.Throws<ArgumentNullException>(() => EncryptUtility.AESEncrypt("stetsaasd", null, "70(*&^%$#@hsac1q"));
+            Assert.Throws<ArgumentNullException>(() => EncryptUtility.AESEncrypt(null, "sdhqjekalsoeiwjqna)2(12kfiuwqdf1", "70(*&^%$#@hsac1q"));
+            Assert.Throws<ArgumentNullException>(() => EncryptUtility.AESEncrypt("stetsaasd", "sdhqjekalsoeiwjqna)2(12kfiuwqdf1", null));
+
+
+            Assert.Throws<ArgumentException>(() => EncryptUtility.AESEncrypt("stetsaasd", "sdhqjekalsoeiwjqna)2(12kfiuwqdf1", key2.ToString()));
+            Assert.Throws<ArgumentException>(() => EncryptUtility.AESEncrypt("stetsaasd", key1.ToString(), "70(*&^%$#@hsac1q"));
+
+            var encrypt = EncryptUtility.AESEncrypt(key1.ToString(), "sdhqjekalsoeiwjqna)2(12kfiuwqdf1", "70(*&^%$#@hsac1q");
+            var decrypt = EncryptUtility.AESDecrypt(encrypt, "sdhqjekalsoeiwjqna)2(12kfiuwqdf1", "70(*&^%$#@hsac1q");
+            Assert.Equal(key1.ToString(), decrypt);
+
+            Assert.Throws<ArgumentNullException>(() => EncryptUtility.AESDecrypt("stetsaasd", null, "70(*&^%$#@hsac1q"));
+            Assert.Throws<ArgumentNullException>(() => EncryptUtility.AESDecrypt(null, "sdhqjekalsoeiwjqna)2(12kfiuwqdf1", "70(*&^%$#@hsac1q"));
+            Assert.Throws<ArgumentNullException>(() => EncryptUtility.AESDecrypt("stetsaasd", "sdhqjekalsoeiwjqna)2(12kfiuwqdf1", null));
+
+
+            Assert.Throws<ArgumentException>(() => EncryptUtility.AESDecrypt("stetsaasd", "sdhqjekalsoeiwjqna)2(12kfiuwqdf1", key2.ToString()));
+            Assert.Throws<ArgumentException>(() => EncryptUtility.AESDecrypt("stetsaasd", key1.ToString(), "70(*&^%$#@hsac1q"));
+
+        }
+    }
+}
---
+++ Abner.Common.Utility/EncryptHelper/EncryptUtility.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Abner.Common.Utility
+{
+    public class EncryptUtility
+    {
+        /// <summary>
+        /// AES加密
+        /// </summary>
+        /// <param name="plainText"></param>
+        /// <param name="key"></param>
+        /// <param name="IV"></param>
+        /// <returns>返回base64编码后的字符串</returns>
+        /// <exception cref="ArgumentNullException"></exception>
+        /// <exception cref="ArgumentException"></exception>
+        public static string AESEncrypt(string plainText, string key, string IV)
+        {
+            // Check arguments.
+            if (plainText == null || plainText.Length <= 0)
+                throw new ArgumentNullException("plainText");
+            if (key == null)
+                throw new ArgumentNullException("Key");
+            if (key.Length != 32)
+                throw new ArgumentException("Key's Length is not 32");
+            if (IV == null)
+                throw new ArgumentNullException("IV");
+            if (IV.Length != 16)
+                throw new ArgumentException("IV's Length is not 16");
+            byte[] encrypted;
+
+            // Create an Aes object
+            // with the specified key and IV.
+            using (Aes aesAlg = Aes.Create())
+            {
+                aesAlg.Key = Encoding.UTF8.GetBytes(key);
+                aesAlg.IV = Encoding.UTF8.GetBytes(IV);
+
+                // Create an encryptor to perform the stream transform.
+                ICryptoTransform encryptor = aesAlg.CreateEncryptor(aesAlg.Key, aesAlg.IV);
+
+                // Create the streams used for encryption.
+                using (MemoryStream msEncrypt = new MemoryStream())
+                {
+                    using (CryptoStream csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write))
+                    {
+                        using (StreamWriter swEncrypt = new StreamWriter(csEncrypt))
+                        {
+                            //Write all data to the stream.
+                            swEncrypt.Write(plainText);
+                        }
+                        encrypted = msEncrypt.ToArray();
+                    }
+                }
+            }
+
+            return Convert.ToBase64String(encrypted);
+        }
+
+        /// <summary>
+        /// AES解密
+        /// </summary>
+        /// <param name="cipherText">base64编码的字符串</param>
+        /// <param name="key"></param>
+        /// <param name="IV"></param>
+        /// <returns></returns>
+        /// <exception cref="ArgumentNullException"></exception>
+        /// <exception cref="ArgumentException"></exception>
+        public static string AESDecrypt(string cipherText, string key, string IV)
+        { 
+            // Check arguments.
+            if (cipherText == null || cipherText.Length <= 0)
+                throw new ArgumentNullException("cipherText");
+            if (key == null)
+                throw new ArgumentNullException("Key");
+            if (key.Length != 32)
+                throw new ArgumentException("Key's Length is not 32");
+            if (IV == null)
+                throw new ArgumentNullException("IV");
+            if (IV.Length != 16)
+                throw new ArgumentException("IV's Length is not 16");
+
+            // Declare the string used to hold
+            // the decrypted text.
+            string plaintext = null;
+
+            // Create an Aes object
+            // with the specified key and IV.
+            using (Aes aesAlg = Aes.Create())
+            {
+                aesAlg.Key = Encoding.UTF8.GetBytes(key);
+                aesAlg.IV = Encoding.UTF8.GetBytes(IV);
+
+                // Create a decryptor to perform the stream transform.
+                ICryptoTransform decryptor = aesAlg.CreateDecryptor(aesAlg.Key, aesAlg.IV);
+
+                // Create the streams used for decryption.
+                using (MemoryStream msDecrypt = new MemoryStream(Convert.FromBase64String(cipherText)))
+                {
+                    using (CryptoStream csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Read))
+                    {
+                        using (StreamReader srDecrypt = new StreamReader(csDecrypt))
+                        {
+
+                            // Read the decrypted bytes from the decrypting stream
+                            // and place them in a string.
+                            plaintext = srDecrypt.ReadToEnd();
+                        }
+                    }
+                }
+            }
+
+            return plaintext;
+        }
+    }
+}
